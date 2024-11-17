49fd743d7541c4514289703d908ac537a6f8c65e
Alexey Vymyatnin <avymiatnin@aspirity.com>
2024-11-17 23:49:47 +0700

    Rewrite eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,48 +1,68 @@
-import eslint from '@eslint/js';
-import globals from 'globals';
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config({
-  files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-  ignores: ['dist/**', 'node_modules/**', 'bin/**', 'build/**'],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-  plugins: {
-    react,
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
+export default [
+  {
+    ignores: ['node_modules', 'dist', 'build', 'vite.config.ts'],
   },
-  rules: {
-    ...eslint.configs.recommended.rules,
-    ...react.configs.recommended.rules,
-    ...reactHooks.configs.recommended.rules,
-    '@typescript-eslint/array-type': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'no-unused-vars': 'warn',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
-  settings: {
-    react: {
-      version: '18.3',
-    },
-  },
-  languageOptions: {
-    ecmaVersion: 'latest',
-    globals: globals.browser,
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...typescript.configs['recommended-requiring-type-checking'].rules,
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
-});
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    rules: {
+      ...prettier.rules,
+      'prettier/prettier': ['error'],
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: {
+        version: '18.0', // Replace with your React version
+      },
+    },
+  },
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import './App.css';
+import React from 'react';
 import Counter from './Counter';
+import './App.css';
 
 function App() {
   return (
